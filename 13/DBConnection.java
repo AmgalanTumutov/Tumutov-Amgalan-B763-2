fda3cd360654c7435d8f75f859d7aac846b79daa
AmgalanTumutov <atumutov@list.ru>
2024-06-18 17:03:52 +0800

    Update DBConnection.java

--- 13/DBConnection.java
+++ 13/DBConnection.java
@@ -3,12 +3,26 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-
-    private static final String url = "jdbc:mysql://localhost:3306/diction";
-    private static final String username = "root";
-    private static final String password = "password";
-
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(url, username, password);
+    private static DBConnection instance;
+    private static Connection connection;
+    private DBConnection() {
+        try {
+            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/DICTION", "root", "password");
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка создания соединения с БД.", e);
+        }
+    }
+    public static DBConnection getInstance() {
+        if (instance == null) {
+            synchronized (DBConnection.class) {
+                if (instance == null) {
+                    instance = new DBConnection();
+                }
+            }
+        }
+        return instance;
+    }
+    public static Connection getConnection() {
+        return connection;
     }
 }
