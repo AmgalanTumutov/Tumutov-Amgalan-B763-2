866a421625826e22c421a5f7f13c2abc17a50ce6
AmgalanTumutov <atumutov@list.ru>
2024-06-18 15:31:03 +0800

    Add files via upload

---
+++ 8/Main.java
@@ -0,0 +1,33 @@
+/*
+Тумутов Амгалан Б763-2
+Вариант А
+18. В тексте определить все согласные буквы, встречающиеся не более чем в двух словах.
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        String text = "Пример текста";
+        Map<Character, Integer> consonantsCount = new HashMap<>();
+        String consonants = "бвгджзйклмнпрстфхцчшщ";
+
+        for (int i = 0; i < text.length(); i++) {
+            char currentChar = Character.toLowerCase(text.charAt(i));
+            if (consonants.contains(String.valueOf(currentChar))) {
+                consonantsCount.put(currentChar, consonantsCount.getOrDefault(currentChar, 0) + 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : consonantsCount.entrySet()) {
+            if (entry.getValue() <= 2) {
+                System.out.println("Согласная буква \"" + entry.getKey() + "\" встречается не более чем в двух словах");
+            }
+        }
+    }
+}
+
+
+
+
