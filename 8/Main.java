d9633aefeaf4d9b9217c1dfb33c665d85441df35
AmgalanTumutov <atumutov@list.ru>
2024-06-19 16:41:20 +0800

    Update Main.java

--- 8/Main.java
+++ 8/Main.java
@@ -5,26 +5,32 @@
 */
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         String text = "ВСЁ ЖИВОЕ БОИТСЯ МУЧЕНИЯ, ВСЁ ЖИВОЕ БОИТСЯ СМЕРТИ; ПОЙМИ САМОГО СЕБЯ ВО ВСЯКОМ ЖИВОМ СУЩЕСТВЕ, НЕ МУЧАЙ И НЕ УБИВАЙ, НЕ ПРИЧИНЯЙ СТРАДАНИЙ И СМЕРТИ.";
-        Map<Character, Integer> consonantsCount = new HashMap<>();
+        Map<Character, Set<String>> consonantToWordsMap = new HashMap<>();
         String consonants = "бвгджзйклмнпрстфхцчшщ";
-
-        for (int i = 0; i < text.length(); i++) {
-            char currentChar = Character.toLowerCase(text.charAt(i));
-            if (consonants.contains(String.valueOf(currentChar))) {
-                consonantsCount.put(currentChar, consonantsCount.getOrDefault(currentChar, 0) + 1);
+        String[] words = text.toLowerCase().split("\\s+");
+        for (String word : words) {
+            Set<Character> uniqueConsonantsInWord = new HashSet<>();
+            for (char ch : word.toCharArray()) {
+                if (consonants.indexOf(ch) != -1) {
+                    uniqueConsonantsInWord.add(ch);
+                }
+            }
+            for (char consonant : uniqueConsonantsInWord) {
+                consonantToWordsMap.computeIfAbsent(consonant, k -> new HashSet<>()).add(word);
             }
         }
-
-        for (Map.Entry<Character, Integer> entry : consonantsCount.entrySet()) {
-            if (entry.getValue() <= 2) {
-                System.out.println("Согласная буква \"" + entry.getKey() + "\" встречается не более чем в двух словах");
+        consonantToWordsMap.forEach((consonant, wordsSet) -> {
+            if (wordsSet.size() <= 2) {
+                System.out.println("Согласная буква \"" + consonant + "\" встречается не более чем в двух словах: " + wordsSet);
             }
-        }
+        });
     }
 }
 
