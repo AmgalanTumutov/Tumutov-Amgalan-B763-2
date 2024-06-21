d9b80a6861b823f8c49957dda46c4ee5f059a6d9
AmgalanTumutov <atumutov@list.ru>
2024-06-21 20:53:39 +0800

    Add files via upload

---
+++ 1/Terminal.java
@@ -0,0 +1,15 @@
+public class Terminal {
+    private int terminalNumber;
+
+    public Terminal(int number) {
+        this.terminalNumber = number;
+    }
+
+    public void embarkPassenger(Passenger passenger, Aircraft aircraft) {
+        // Логика посадки пассажира на самолет
+    }
+
+    public void disembarkPassenger(Passenger passenger, Aircraft aircraft) {
+        // Логика высадки пассажира из самолета
+    }
+}
