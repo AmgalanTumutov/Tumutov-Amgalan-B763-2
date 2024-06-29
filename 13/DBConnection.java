e5c3caec80aa35ac1a17bbcabedc71edc21e5289
AmgalanTumutov <atumutov@list.ru>
2024-06-29 21:51:02 +0800

    Update DBConnection.java

--- 13/DBConnection.java
+++ 13/DBConnection.java
@@ -3,26 +3,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static DBConnection instance;
-    private static Connection connection;
-    private DBConnection() {
-        try {
-            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/DICTION", "root", "password");
-        } catch (SQLException e) {
-            throw new RuntimeException("Ошибка создания соединения с БД.", e);
-        }
-    }
-    public static DBConnection getInstance() {
-        if (instance == null) {
-            synchronized (DBConnection.class) {
-                if (instance == null) {
-                    instance = new DBConnection();
-                }
-            }
-        }
-        return instance;
-    }
+    private static final String URL = "jdbc:mysql://localhost:3306/SLOV";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "password";
+
     public static Connection getConnection() {
-        return connection;
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
